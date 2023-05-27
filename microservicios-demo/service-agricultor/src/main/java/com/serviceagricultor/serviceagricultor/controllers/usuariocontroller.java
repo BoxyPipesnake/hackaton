02e80b5af5ca1a02dd3e14ed19508657e992eeca
Mrkaponi <mrkaponi@outlook.es>
2023-05-27 01:24:29 -0600

    Update usuariocontroller.java

--- microservicios-demo/service-agricultor/src/main/java/com/serviceagricultor/serviceagricultor/controllers/usuariocontroller.java
+++ microservicios-demo/service-agricultor/src/main/java/com/serviceagricultor/serviceagricultor/controllers/usuariocontroller.java
@@ -1,4 +1,9 @@
 package com.serviceagricultor.serviceagricultor.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api-cultivo")
 public class usuariocontroller {
 }
