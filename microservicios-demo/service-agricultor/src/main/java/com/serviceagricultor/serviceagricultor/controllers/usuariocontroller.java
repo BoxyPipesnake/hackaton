4f0d55a7746e1e60adeba7a7a4a8aa7089f920d8
Mrkaponi <mrkaponi@outlook.es>
2023-05-27 11:56:47 -0600

    servicio-usuario

--- microservicios-demo/service-agricultor/src/main/java/com/serviceagricultor/serviceagricultor/controllers/usuariocontroller.java
+++ microservicios-demo/service-agricultor/src/main/java/com/serviceagricultor/serviceagricultor/controllers/usuariocontroller.java
@@ -1,13 +1,14 @@
 package com.serviceagricultor.serviceagricultor.controllers;
 
+import com.serviceagricultor.serviceagricultor.model.entity.cultivo;
+import com.serviceagricultor.serviceagricultor.model.service.impCultivo;
 import com.serviceagricultor.serviceagricultor.model.entity.usuario;
 import com.serviceagricultor.serviceagricultor.model.service.impUsuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api-cultivo")
@@ -15,13 +16,25 @@ public class usuariocontroller {
     @Autowired
     private impUsuario imp;
 
+    @Autowired
+    private impCultivo cultivoimp;
+
     @PostMapping("/save")
     public ResponseEntity<?> guardar(@RequestBody usuario us) {
 
-        imp.save(us);
+
 
 
 
         return ResponseEntity.ok(us);
     }
+
+
+    @GetMapping("/findAll")
+    public List<usuario> findall() {
+
+        return imp.findAll();
+    }
+
+
 }
