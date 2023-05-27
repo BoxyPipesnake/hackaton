01600921945f2df1f0be2d3f8405e20b115a8cc8
Mrkaponi <mrkaponi@outlook.es>
2023-05-26 23:28:42 -0600

    servicio-agricultor

---
+++ microservicios-demo/service-agricultor/src/main/java/com/serviceagricultor/serviceagricultor/ServiceAgricultorApplication.java
@@ -0,0 +1,13 @@
+package com.serviceagricultor.serviceagricultor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceAgricultorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceAgricultorApplication.class, args);
+	}
+
+}
---
+++ microservicios-demo/service-agricultor/src/test/java/com/serviceagricultor/serviceagricultor/ServiceAgricultorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serviceagricultor.serviceagricultor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceAgricultorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
